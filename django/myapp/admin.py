4c6ac8ee95756fe92bf7fa344ea2099d8f215cb1
Raheek_Helal <helalraheek@gmail.com>
2023-12-10 18:41:29 +0200

    add the API

---
+++ django/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Lawyer)
+admin.site.register(Courts)
+admin.site.register(Cases)
+admin.site.register(Post)
